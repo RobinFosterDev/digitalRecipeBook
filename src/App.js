1ccb58cd6203234e88041aabc7fca64e2edb8f1a
RobinFosterDev <45547133+RobinFosterDev@users.noreply.github.com>
2020-08-07 17:07:38 +1000

    Created Recipe Component
    Created the recipe component and sourced data from an API.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipes';
 
-function App() {
+const App = () => {
+  const APP_ID = '420135a0';
+  const APP_KEY = '157fbf5ab78daae9bdfcf9aaa4ef51bd';
+
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe Time</h1>
+      <form className = "searchForm"> 
+        <input type="text" className="searchBar"></input>
+        <button type="submit" className="submitButton">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.recipe.label} ingredients={recipe.recipe.ingredientLines} method={recipe.recipe.method} image={recipe.recipe.image} />
+      ))}
     </div>
   );
 }
---
+++ src/Recipes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Recipe = ({title, ingredients, image}) => {
+    return(
+        <div className='recipe'>
+            <h1>{title}</h1>
+            <img src={image} alt=""></img>
+            <h2>Ingredients</h2>
+            <p>{ingredients}</p>
+        </div>
+    )
+};
+
+export default Recipe;
